5a32c669c5d9f39516d64735fa0783976dbd6c73
sergentum <sergentums@gmail.com>
2018-05-08 12:07:08 +0700

    asd

--- src/main/java/org/app/web/CarController.java
+++ src/main/java/org/app/web/CarController.java
@@ -1,7 +1,5 @@
 package org.app.web;
 
-import org.app.model.CarEntity;
-import org.app.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,35 +15,9 @@ import java.util.stream.Collectors;
 @Controller
 public class CarController {
 
-    @Autowired
-    private CarService carService;
-
     @RequestMapping(value = "/cars")
-    public String listCars(Model model) {
-        model.addAttribute("carEntity", new CarEntity());
-        List<CarEntity> listCars = carService.getAll();
-        model.addAttribute("listCars", listCars.stream()
-                .sorted(Comparator.comparing(CarEntity::getId))
-                .collect(Collectors.toList()));
-        return "cars";
-    }
-
-    @RequestMapping(value = "/cars/add", method = RequestMethod.POST)
-    public String addCar(@ModelAttribute("carEntity") CarEntity car) {
-        carService.save(car);
-        return "redirect:/cars";
-    }
-
-    @RequestMapping("/cars/remove/{id}")
-    public String removeCar(@PathVariable("id") long id) {
-        carService.remove(id);
-        return "redirect:/cars";
-    }
-
-    @RequestMapping("/cars/edit/{id}")
-    public String editCar(@PathVariable("id") long id, Model model) {
-        model.addAttribute("carEntity", carService.getById(id));
-        model.addAttribute("listCars", carService.getAll());
+    public String listCars() {
+        System.out.println("cars get");
         return "cars";
     }
 }
